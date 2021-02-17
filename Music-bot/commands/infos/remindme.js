2337dd872789308b55272251d4684c90cd33a293
Alejandro Valero <64314663+ALX-00@users.noreply.github.com>
2021-02-17 20:15:30 +0100

    Add files via upload
    created command remindme

---
+++ Music-bot/commands/infos/remindme.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'remindme',
+    aliases: [],
+    category: 'Infos',
+    utilisation: '{prefix}remindme',
+
+    execute(client, message,args) {
+        if (args.lenght == 0)
+            log(`aa`);
+    },
+};

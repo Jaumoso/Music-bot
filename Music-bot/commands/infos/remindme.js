5a6f86c39d87d757913a398c79c821d751a1a5f6
Alejandro Valero <64314663+ALX-00@users.noreply.github.com>
2021-02-17 20:51:22 +0100

    Add files via upload

--- Music-bot/commands/infos/remindme.js
+++ Music-bot/commands/infos/remindme.js
@@ -4,8 +4,22 @@ module.exports = {
     category: 'Infos',
     utilisation: '{prefix}remindme',
 
-    execute(client, message,args) {
-        if (args.lenght == 0)
-            message.channel.send(`¿Qué quieres que te recuerde?`);
+    execute(client, message) {
+        // accepted messages will be those from the same author, we compare IDs to make sure
+        const filter = msg => msg.author.id == message.author.id;
+        // Errors: ['time'] treats ending because of the time limit as an error
+        channel.awaitMessages(filter, { max: 4, time: 60000, errors: ['time'] })
+        .then(collected => console.log(collected.size))
+        .catch(collected => console.log(`After a minute, only ${collected.size} out of 4 voted.`));
+
+        // request
+        message.channel.send("¿Qué quieres que te recuerde?");
+        
+        // collector
+        let collector = await message.channel.awaitMessages(filter, options);
+        let answer = collector.first().content;
+        
+        // response
+        await message.reply("Te recordaré que" + answer + "!");
     },
 };
